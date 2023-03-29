548d8e8049f78b1da67622cdfdb55727b531fd30
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2023-03-29 16:17:16 +0200

    refactor: Class properties - Due to having remade the graphical interface from scratch using flet, tkinter's "StringVar" and "BooleanVar" no longer worked. - So what I've done is remove the "StringVar" and the "BooleanVar" from tkinter by class properties with getter and setter methods

--- src/app_logic/control_variables.py
+++ src/app_logic/control_variables.py
@@ -1,62 +1,69 @@
 """Control variables for the operation of the app"""
 
-from tkinter import StringVar, BooleanVar
-
-VIDEO_LOCATION = StringVar()
-LINK_VIDEO = StringVar()
-DOWNLOAD_NAME = StringVar()
-DOWNLOADED_SUCCESSFULLY = BooleanVar()
-
 
 class ControlVariables:
     """Control variables for the app"""
 
-    @staticmethod
-    def get_location_video():
+    def __init__(self):
+        self._video_location = ""
+        self._url_video = ""
+        self._download_name = ""
+        self._downloaded_successfully = None
+
+    @property
+    def video_location(self):
         """
         Returns the location selected by the user
         :return: The location selected by the user
         """
 
-        return VIDEO_LOCATION.get()
+        return self._video_location
 
-    @staticmethod
-    def get_download_name():
+    @video_location.setter
+    def video_location(self, location):
+        self._video_location = location
+
+    @property
+    def download_name(self):
         """
         Returns the name of the download
         :return: The name of the download
         """
 
-        return DOWNLOAD_NAME.get()
+        return self._download_name
 
-    @staticmethod
-    def set_download_name(name):
+    @download_name.setter
+    def download_name(self, name):
         """Set the name for the downloaded video"""
 
-        DOWNLOAD_NAME.set(name)
+        self._download_name = name
 
-    @staticmethod
-    def set_downloaded_successfully(downloaded):
-        """
-        Establishes if a videoaudio has been downloaded well or not
-        """
-
-        DOWNLOADED_SUCCESSFULLY.set(downloaded)
-
-    @staticmethod
-    def get_downloaded_successfully():
+    @property
+    def downloaded_successfully(self):
         """
         Returns if a video has been downloaded well or not
         :return: Whether a video downloaded well or not
         """
 
-        return DOWNLOADED_SUCCESSFULLY.get()
+        return self._downloaded_successfully
 
-    @staticmethod
-    def get_url_video():
+    @downloaded_successfully.setter
+    def downloaded_successfully(self, downloaded):
+        """
+        Establishes if a video or audio has been downloaded well or not
+        """
+
+        self._downloaded_successfully = downloaded
+
+    @property
+    def url_video(self):
         """
         Returns the url of the video
         :return: The url of the video
         """
 
-        return LINK_VIDEO.get()
+        return self._url_video
+
+    @url_video.setter
+    def url_video(self, url):
+        self._url_video = url
