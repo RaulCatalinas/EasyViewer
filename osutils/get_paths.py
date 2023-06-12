434d95997909f922dd2b52f028d57370c2788f3a
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2023-06-12 12:18:18 +0200

    chore: Optimized imports - Unnecessary imports have been eliminated, with this we've managed to improve the performance of the app a bit, since we're no longer importing things that we don't use

--- osutils/get_paths.py
+++ osutils/get_paths.py
@@ -23,23 +23,6 @@ class GetPaths:
         return DESKTOP_PATH
 
     @classmethod
-    def get_icon_path(cls):
-        """
-        Returns the path to the icon in the appropriate format based on the operating system.
-
-        :param relative_root_path: The relative path to the root folder (optional)
-
-        :return: The path to the icon is returned in the appropriate format depending on the operating system. On macOS, the icon is returned in ICNS format; on Windows, in ICO format; and on Linux, in the original format (PNG).
-        """
-
-        from utils import SYSTEM_NAME, ICONS
-
-        if SYSTEM_NAME not in ICONS:
-            raise ValueError("Unsupported operating system")
-
-        return ICONS[SYSTEM_NAME]
-
-    @classmethod
     def get_config_file(cls, config_file: str):
         """
         Returns a configuration file based on the input string parameter.
