27ff44c6867007836ab5732773c5c96c24e69157
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2023-06-12 13:22:03 +0200

    chore: Improved icon quality - The quality of the anthem has been improved when the app was running, before when the app was running the icon didn't have good quality
    - So, to improve what has been done is to have the icon in png format, and then when compiling the app using pillow it's automatically converted to the corresponding icon format of the OS for which the app is being compiled.

--- build.py
+++ build.py
@@ -1,8 +1,7 @@
 """Build the app"""
 
-from subprocess import run
+from subprocess import run, CalledProcessError
 
-from osutils import GetPaths
 from utils import ENABLED_TYPE_CHECKING
 
 __version__ = "2.0.0"
@@ -14,16 +13,16 @@ def build():
     if not ENABLED_TYPE_CHECKING:
         print("Type checking off")
 
-    icon = GetPaths.get_icon_path()
+    command = f"""flet pack src/main.py -n EasyViewer -i icon/icon.png --product-name EasyViewer --file-description "App to download youtube videos" --file-version {__version__} --product-version {__version__} --copyright "Copyright (c) 2023 Raul Catalinas Esteban" --add-data config;config --add-data icon;icon --add-data src;src --add-data control;control --add-data osutils;osutils --add-data utils;utils"""
 
-    COMMAND = f"""flet pack src/main.py -n EasyViewer -i {icon} --product-name EasyViewer --file-description "App to download youtube videos" --file-version {__version__} --product-version {__version__} --copyright "Copyright (c) 2023 Raul Catalinas Esteban" --add-data config;config --add-data "{icon};icon" --add-data src;src --add-data control;control --add-data osutils;osutils --add-data utils;utils"""
+    try:
+        run(command, capture_output=True, check=True)
 
-    execute = run(COMMAND, capture_output=True, check=True)
+    except CalledProcessError as e:
+        print(f"Could not compile, this is the reason: {e.stderr}")
 
-    if execute.returncode == 0:
-        return print("App compiled successfully")
-
-    return print(f"Could not compile, this is the reason: {execute.stderr}")
+    else:
+        print("App compiled successfully")
 
 
 build()
