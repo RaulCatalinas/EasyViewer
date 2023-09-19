eb0898bc21fb476526b10862b75da51437aa7682
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2023-09-20 01:11:38 +0200

    chore: Moved configuration files to 'settings_files' folder
    - Relocated all configuration files to the 'settings_files' folder for better organisation and maintenance of the project.

--- src/constants/paths.py
+++ src/constants/paths.py
@@ -8,8 +8,8 @@ USER_HOME = GetPaths.get_user_home_path()
 DESKTOP_PATH = USER_HOME.joinpath("Desktop")
 
 CONFIG_FILES = {
-    "INI": ROOT_PATH.joinpath("settings/control_variables.ini"),
-    "JSON": ROOT_PATH.joinpath("settings/settings.json"),
-    "EXCEL": ROOT_PATH.joinpath("settings/languages.xlsx"),
-    "ENV": ROOT_PATH.joinpath("settings/.env"),
+    "INI": ROOT_PATH.joinpath("settings_files/control_variables.ini"),
+    "JSON": ROOT_PATH.joinpath("settings_files/settings.json"),
+    "EXCEL": ROOT_PATH.joinpath("settings_files/languages.xlsx"),
+    "ENV": ROOT_PATH.joinpath("settings_files/.env"),
 }
--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -2,13 +2,13 @@
 Gets the app's configuration
 """
 # Envrioment variables
-from .modules.environment_variables import EnvironmentVariables
+from .environment_variables import EnvironmentVariables
 
 # Excel text loader
-from .modules.excel_text_loader import ExcelTextLoader
+from .excel_text_loader import ExcelTextLoader
 
 # Get config JSON
-from .modules.get_config_json import GetConfigJson
+from .get_config_json import GetConfigJson
 
 __all__ = [
     "ExcelTextLoader",
