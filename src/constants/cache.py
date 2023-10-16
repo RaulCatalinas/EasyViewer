6242a21e1430ea121ebf1ad7ce85974827f98f64
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2023-10-16 16:57:36 +0200

    fix: Resolve issue with missing 'github_cache.json' file
    - Previously, the 'github_cache.json' file could not be found on the user's PC because the 'src' folder was not included in the executable.
    - To address this, I've updated the code to use the 'ROOT_PATH' constant, which holds the project's root path, and appended 'backend/cache/github_cache.json' to it. This change ensures the correct file path and resolves the issue.

--- src/constants/cache.py
+++ src/constants/cache.py
@@ -1,3 +1,5 @@
-CACHE_FILE = "./src/backend/cache/github_cache.json"
+from .paths import ROOT_PATH
+
+CACHE_FILE = ROOT_PATH.joinpath("backend/cache/github_cache.json")
 
 DAYS_FOR_THE_CACHE_TO_EXPIRE = 30
