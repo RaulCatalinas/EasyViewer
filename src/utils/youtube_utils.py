0ead7d86494299dc6860d862a8c7f3e0b11ebfa9
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2023-10-23 18:52:50 +0200

    docs: Added missing docstrings
    - This commit includes the addition of missing docstrings to improve code documentation.

--- src/utils/youtube_utils.py
+++ src/utils/youtube_utils.py
@@ -4,12 +4,29 @@ from re import sub
 # Strings utils
 from .strings_utils import remove_empty_strings
 
-# Check type
-from .type_checker import check_type
 
-
-@check_type
 def separate_urls(urls: str):
+    """
+    Separate URLs in a string and return them as a list.
+
+    Takes a string containing URLs and separates them into a list.
+
+    It first adds a space before each "https://" to ensure proper separation and then splits the string into individual URLs using line breaks.
+
+    It also removes any empty strings that might result from the splitting.
+
+    Args:
+        urls (str): A string containing URLs.
+
+    Returns:
+        list[str]: A list of separated URLs.
+
+    Example:
+        >>> urls = "https://example.com https://another-url.comhttps://another-url.com"
+        >>> separate_urls(urls)
+        ['https://example.com', 'https://another-url.com', 'https://another-url.com']
+    """
+
     urls_with_spaces = sub(r"(https://)", r" \1", urls).replace(" ", "\n")
 
     return remove_empty_strings(urls_with_spaces.splitlines())
