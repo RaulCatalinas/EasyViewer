f6150fb4a382950ac8f6efe1c3fba1deb2b59df3
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2025-02-15 01:04:37 +0100

    feat: add "AppSettings" class to manage app configuration options
    - Created "AppSettings" class to centralize and manage application settings.

---
+++ src/app_settings/config.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class AppSettings(Enum):
+    TITLE = "EasyViewer"
+    WIDTH = 830
+    HIGH = 575
+    PROGRESS_BAR_COLOR = "00FF00"
+    PREVENT_CLOSE = True
+    RESIZABLE = False
+    MAXIMIZABLE = False
--- src/managers/window_manager.py
+++ src/managers/window_manager.py
@@ -4,9 +4,12 @@ from typing import Callable
 # Third party libraries
 from flet import Page, WindowEvent
 
+# App settings
+from app_settings import AppSettings
+
 
 class WindowManager:
-    """Clase encargada de manejar la configuración de la ventana."""
+    """Class in charge of managing the window configuration."""
 
     def __init__(
         self, app: Page, events_handler: Callable[[WindowEvent], None]
@@ -16,13 +19,13 @@ class WindowManager:
         self._configure_window()
 
     def _configure_window(self):
-        """Configura las propiedades de la ventana."""
+        """Set up the app window."""
         self.app.window.wait_until_ready_to_show = True
         self.app.window.center()
-        self.app.window.resizable = False
-        self.app.window.maximizable = False
-        self.app.window.width = 830
-        self.app.window.height = 575
-        self.app.title = "EasyViewer"
-        self.app.window.prevent_close = True
+        self.app.window.resizable = AppSettings.RESIZABLE.value
+        self.app.window.maximizable = AppSettings.MAXIMIZABLE.value
+        self.app.window.width = AppSettings.WIDTH.value
+        self.app.window.height = AppSettings.HIGH.value
+        self.app.title = AppSettings.TITLE.value
+        self.app.window.prevent_close = AppSettings.PREVENT_CLOSE.value
         self.app.window.on_event = self.events_handler
