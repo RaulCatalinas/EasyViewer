60813917c0819772ab7b5809f7516e759cb9a090
Raul Catalinas <raulcatalinas@gmail.com>
2025-11-10 23:40:24 +0100

    chore: File core_utils.dart created

---
+++ lib/core/core_utils.dart
@@ -0,0 +1,15 @@
+import 'package:youtube_explode_dart/solvers.dart' show DenoEJSSolver;
+import 'package:youtube_explode_dart/youtube_explode_dart.dart'
+    show YoutubeExplode;
+
+YoutubeExplode? _youtubeExplodeInstance;
+
+Future<YoutubeExplode> getYoutubeExplodeInstance() async {
+  if (_youtubeExplodeInstance != null) return _youtubeExplodeInstance!;
+
+  final solver = await DenoEJSSolver.init();
+
+  _youtubeExplodeInstance = YoutubeExplode(jsSolver: solver);
+
+  return _youtubeExplodeInstance!;
+}
